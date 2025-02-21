341b8af7935224ef004165542da237c66ef76c7a
Serhii Savchuk <serhii.savchuk.dev@gmail.com>
2025-02-20 20:22:25 -0500

    v3: changed version

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-import skills from "./skills.js";
-import slides from "./slides.mjs";
-// import sideMenu from "./side-menu.mjs";
-
-(function () {
-  skills();
-  slides();
-  // sideMenu();
-})();
